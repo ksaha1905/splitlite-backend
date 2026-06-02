6f44b06329f0ff936681c2d19bc35834dbacc5b7
Kaushik Saha <ksaha1905@gmail.com>
2026-06-02 16:26:40 +0530

    chore: configure app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable cors
+
+  app.enableCors();
+
+
+  // Global validation
+
+  app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }),
+);
+
+
+// Swagger setup
+
+const config = new DocumentBuilder()
+  .setTitle('Splitlite API')
+  .setDescription('Expense splitting API')
+  .setVersion('1.0')
+  .build();
+
+const document = SwaggerModule.createDocument(
+  app,
+  config,
+);
+
+SwaggerModule.setup('api', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
