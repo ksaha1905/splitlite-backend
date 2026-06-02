f4414d8700bba26ea546d67971eed4e0a5bbce8e
Kaushik Saha <ksaha1905@gmail.com>
2026-06-02 16:08:37 +0530

    feat: setup prisma and connect neon database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule
   ],
   controllers: [],
   providers: [],
