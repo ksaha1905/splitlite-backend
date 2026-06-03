14f349668e0443234e55f8a76cf31c0e0b83fa32
Kaushik Saha <ksaha1905@gmail.com>
2026-06-03 16:18:07 +0530

    feat: implement supabase jwt strategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import * as jwksRsa from 'jwks-rsa';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt',
+) {
+  constructor(configService: ConfigService) {
+    const supabaseUrl =
+      configService.get<string>('SUPABASE_URL');
+
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+      issuer: `${supabaseUrl}/auth/v1`,
+
+      audience: 'authenticated',
+
+      algorithms: ['RS256'],
+
+      secretOrKeyProvider: jwksRsa.passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+
+        jwksUri: `${supabaseUrl}/auth/v1/.well-known/jwks.json`,
+      }) as any,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
