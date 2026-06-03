1e4f84e335bd580d9f340f8d19556973b364deac
Kaushik Saha <ksaha1905@gmail.com>
2026-06-03 16:15:17 +0530

    chore: auth module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    PrismaModule
+    PrismaModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
