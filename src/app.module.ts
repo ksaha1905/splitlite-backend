d98ced02c4d889b37f9236ce16993620c4368961
Kaushik Saha <ksaha1905@gmail.com>
2026-06-08 15:23:23 +0530

    chore: groups module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { GroupsModule } from './modules/groups/groups.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { UsersModule } from './modules/users/users.module';
     }),
     PrismaModule,
     AuthModule,
-    UsersModule
+    UsersModule,
+    GroupsModule
   ],
   controllers: [],
   providers: [],
